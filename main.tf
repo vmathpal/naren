f335d0ec241b49ad89e8a9641b2d5032edeaf986
Aujla Narender Singh (MWZ7VWQ) <MWZ7VWQ@AAD.UPS.com>
2021-04-28 23:31:37 -0400

    my first commit

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "prd-rg" {
+    name = "prod-rg-01"
+    location = "eastus"
+}
